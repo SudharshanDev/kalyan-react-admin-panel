2b08511688ea23ae97a14b0cc8579266f36caa3c
sudhandeveloper <gmsudhan3@gmail.com>
2024-08-18 01:45:21 +0530

    new

--- src/Auth/Login/Index.jsx
+++ src/Auth/Login/Index.jsx
@@ -9,6 +9,8 @@ import {
   OutlinedInput,
 } from "@mui/material";
 import Logo from "../../Images/Logo/kalyan_logo.png";
+import LoginBaground from "../../Images/Baground/bg-gold.jpg";
+import LogoPng from "../../Images/Logo/logo-icon-1.png";
 const Index = () => {
   const [showPassword, setShowPassword] = useState(false);
 
@@ -20,25 +22,30 @@ const Index = () => {
 
   return (
     <div className="flex flex-wrap w-full h-full">
-      <div className="w-1/2 h-[100vh] bg-gradient-to-br from-yellow-300 via-yellow-500 to-orange-500 shadow-lg rounded-r-[40px] flex items-center justify-center">
-        <p className="text-white font-bold text-xl login-glass w-1/2">
-          Diagonal Gradient
-        </p>
+      <div className="w-1/2 relative max-lg:hidden  h-[100vh] bg-black flex items-center justify-center">
+        <img src={LoginBaground} className="object-cover w-full h-full" alt="" />
+        
+        <div className="absolute flex flex-col items-center justify-center text-center h-80 w-72 ">
+          <div className="w-2/12 mb-9">
+          <img src={LogoPng} className="w-full h-full" alt="" />
+          </div>
+          <h1 className="  font-Barlow   w-fit h-fit  font-normal text-white text-[1.4rem]">KALYAN JEWELLERS <br /> Uploade your Products with this Admin Panel</h1>
+        </div>
       </div>
 
       <div className="w-1/2 h-[100vh] ">
-        <div className="h-full w-full flex justify-center items-center p-12">
+        <div className="flex items-center justify-center w-full h-full p-12">
           <div className="w-full">
-            <div className="flex flex-col justify-center  items-center">
-              <div className="h-36 w-8/12 mb-14 flex justify-center items-center">
+            <div className="flex flex-col items-center justify-center">
+              <div className="flex items-center justify-center w-8/12 h-36 mb-14">
                 <img
                   src={Logo}
-                  className="w-fit h-full object-cover"
+                  className="object-cover h-full w-fit"
                   alt="Logo"
                 />
               </div>
               <form className="w-full">
-                <div className="w-full flex flex-col justify-center items-center gap-4 ">
+                <div className="flex flex-col items-center justify-center w-full gap-4 ">
                   <div className="!flex-col flex w-8/12">
                     <label className="mb-[8px]">Enter Password</label>
                     <TextField
@@ -76,7 +83,7 @@ const Index = () => {
                 </div>
               </form>
 
-              <div className="w-8/12 flex justify-between items-center mt-4">
+              <div className="flex items-center justify-between w-8/12 mt-4">
                 <div className="flex items-center ">
                   <Checkbox className="!pl-0 !pt-0 !pb-0 !pr-1" />
                   <span>Remember me</span>
@@ -87,7 +94,7 @@ const Index = () => {
               <div className="w-8/12 rounded-[4px] mt-6 bg-primary py-2 text-center font-semibold text-white">
                 <button>Sign In</button>
               </div>
-              <div className="w-8/12 text-center  mt-9">
+              <div className="w-8/12 text-center mt-9">
                 Don't have an account?
                 <span className="font-semibold">Sign Up</span>
               </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,21 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      
+      fontFamily: {
+        Barlow: ["Roboto", "sans-serif"],
+      },
+      fontSize: {
+        h1: ["36px", { lineHeight: "44px", fontWeight: "700" }],
+        h2: ["32px", { lineHeight: "38px", fontWeight: "700" }],
+        h3: ["24px", { lineHeight: "32px", fontWeight: "700" }],
+        h4: ["22px", { lineHeight: "28px", fontWeight: "700" }],
+        h5: ["20px", { lineHeight: "24px", fontWeight: "700" }],
+        h6: ["18px", { lineHeight: "20px", fontWeight: "700" }],
+      },
       colors: {
         primary: {
           DEFAULT: "#c9a482",
         },
-        
       },
     },
   },
